1bee2ceca508e592a84bb19a2cd74976b65eb721
Joe Allnutt <joe.allnutt@os.uk>
2016-01-22 08:38:56 +0000

    Added attribution to OSM layer

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -33,12 +33,14 @@
         }).setView([51.5252, -0.0902], 15);
 
         var tileUrl = 'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osmAttrib = '&copy; <a href="https://www.openstreetmap.org">OpenStreetMap</a>';
 
         var ostrLayer = L.tileLayer(tileUrl, {
                 id: 'OSTR',
                 minZoom: 7,
                 maxZoom: 18,
-                detectRetina: 'False'});
+                detectRetina: 'False',
+                attribution: osmAttrib});
 
         ostrLayer.addTo(map);
 
