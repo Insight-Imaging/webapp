9535c044f5f50be77f4b5e257cf36d86f8feffe6
Joe Allnutt <joe.allnutt@os.uk>
2016-02-24 09:52:41 +0000

    Added Drawing tools to map

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -36,6 +36,7 @@
 
 
       var googleUrlSat ='http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}';
+
       var googleSat = L.tileLayer(googleUrlSat,{
           id:'GoogleSat',
           maxZoom: 20,
@@ -50,14 +51,6 @@
           subdomains:['mt0','mt1','mt2','mt3'],
           attribution: googleAttrib
       });
-
-      /*Terrain
-
-
-      googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
-          maxZoom: 20,
-          subdomains:['mt0','mt1','mt2','mt3']
-      });*/
 
 
       var osmAttrib = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
@@ -87,6 +80,51 @@
       });
 
       L.control.layers(baseLayers, overlays).addTo(map);
+
+      // attempt to add drawing tools starts here
+      var drawnItems = new L.FeatureGroup();
+      map.addLayer(drawnItems);
+
+      // Set the title to show on the polygon button
+      L.drawLocal.draw.toolbar.buttons.polygon = 'Draw a polygon!';
+
+      var drawControl = new L.Control.Draw({
+        position: 'topright',
+        draw: {
+          circle: {
+            shapeOptions: {
+            color: '#662d91'
+            }
+          },
+          marker: false
+        },
+        edit: {
+          featureGroup: drawnItems,
+          remove: false
+        }
+      });
+
+      map.addControl(drawControl);
+
+      map.on('draw:created', function (e) {
+        var type = e.layerType,
+        layer = e.layer;
+
+        if (type === 'marker') {
+          layer.bindPopup('A popup!');
+        }
+
+        drawnItems.addLayer(layer);
+      });
+
+      map.on('draw:edited', function (e) {
+        var layers = e.layers;
+        var countOfEditedLayers = 0;
+        layers.eachLayer(function(/*layer*/) {
+          countOfEditedLayers++;
+        });
+        
+      });
 
     }
 
